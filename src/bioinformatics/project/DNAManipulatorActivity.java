1d9af733e8b15749e4f7a07fcdd4fc131295ad83
root <root@isk-N73SV.(none)>
2012-03-06 17:59:26 +0000

    Update on the pix

--- src/bioinformatics/project/DNAManipulatorActivity.java
+++ src/bioinformatics/project/DNAManipulatorActivity.java
@@ -2,12 +2,26 @@ package bioinformatics.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DNAManipulatorActivity extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final ImageButton start = (ImageButton) findViewById(R.id.startup_button);
+        
+        start.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+            	start.setVisibility(0);
+            	setContentView(R.layout.menu);	
+            }
+            
+        });
     }
 }

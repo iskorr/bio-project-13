53716f0db151f652d372d94f93b30663c319c226
root <root@isk-N73SV.(none)>
2012-03-09 02:25:16 +0000

    More changes to the 1st Level

--- src/bioinformatics/project/Level1Activity.java
+++ src/bioinformatics/project/Level1Activity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -31,6 +32,9 @@ public class Level1Activity extends Activity {
 
 	// Set of answers to this sequence
 	private ImageView[] genes_ans;
+	
+	// Set of arrows pointing on answers
+	private ImageView[] arrows;
 
 	// Generated sequence of genes
 	private int[] sequence;
@@ -62,6 +66,9 @@ public class Level1Activity extends Activity {
     // The timer and score displays
     private TextView time_disp;
     private TextView score_disp;
+    
+    // Music player
+    private MediaPlayer mp;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -70,17 +77,17 @@ public class Level1Activity extends Activity {
 		initialise();
 		generateSequence();
 		setButtons();
-		
 	}
 
 	// Variables' initialisation
 	private void initialise() {
 		res = this.getResources();
+		mp = MediaPlayer.create(getApplicationContext(), R.raw.tetris);
+		mp.setLooping(true);
+		mp.start();
 		time_disp = (TextView) findViewById(R.id.time);
 		score_disp = (TextView) findViewById(R.id.score);
-		genes = new ImageView[6];
-		genes_ans = new ImageView[6];
-		sequence = new int[ELEM_NUM];
+		initArrays();
 		gen = new Random();
 		timeleft = TIME_LIMIT;
 		score = 0;
@@ -107,7 +114,7 @@ public class Level1Activity extends Activity {
 		};
 		handler.postDelayed(run_seq, DELAY);
 	}
-
+	
 	// Sequence generation
 	public void generateSequence() {
 		for (int i = 0; i < ELEM_NUM; i++) {
@@ -119,31 +126,27 @@ public class Level1Activity extends Activity {
 
 	// Sequence set up
 	public void setGenes() {
-		genes[0] = (ImageView) findViewById(R.id.gene1);
-		genes[1] = (ImageView) findViewById(R.id.gene2);
-		genes[2] = (ImageView) findViewById(R.id.gene3);
-		genes[3] = (ImageView) findViewById(R.id.gene4);
-		genes[4] = (ImageView) findViewById(R.id.gene5);
-		genes[5] = (ImageView) findViewById(R.id.gene6);
 		for (int i = 0; i < ELEM_NUM; i++) {
-			if (sequence[i] == 0) genes[i].setImageDrawable(res.getDrawable(R.drawable.base_c));
-			else if (sequence[i] == 1) genes[i].setImageDrawable(res.getDrawable(R.drawable.base_g));
-			else if (sequence[i] == 2) genes[i].setImageDrawable(res.getDrawable(R.drawable.base_a));
-			else genes[i].setImageDrawable(res.getDrawable(R.drawable.base_t));
-		}
-		genes_ans[0] = (ImageView) findViewById(R.id.gene1_ans);
-		genes_ans[1] = (ImageView) findViewById(R.id.gene2_ans);
-		genes_ans[2] = (ImageView) findViewById(R.id.gene3_ans);
-		genes_ans[3] = (ImageView) findViewById(R.id.gene4_ans);
-		genes_ans[4] = (ImageView) findViewById(R.id.gene5_ans);
-		genes_ans[5] = (ImageView) findViewById(R.id.gene6_ans);
-		for (int i = 0; i < ELEM_NUM; i++) {
-			if (sequence[i] == 0) genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_g));
-			else if (sequence[i] == 1) genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_c));
-			else if (sequence[i] == 2) genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_t));
-			else genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_a));
+			if (sequence[i] == 0) {
+				genes[i].setImageDrawable(res.getDrawable(R.drawable.base_c));
+				genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_g));
+			}
+			else if (sequence[i] == 1) {
+				genes[i].setImageDrawable(res.getDrawable(R.drawable.base_g));
+				genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_c));
+			}
+			else if (sequence[i] == 2) {
+				genes[i].setImageDrawable(res.getDrawable(R.drawable.base_a));
+				genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_t));
+			}
+			else {
+				genes[i].setImageDrawable(res.getDrawable(R.drawable.base_t));
+				genes_ans[i].setImageDrawable(res.getDrawable(R.drawable.base_a));
+			}
 			genes_ans[i].setVisibility(View.INVISIBLE);
+			arrows[i].setVisibility(View.INVISIBLE);
 		}
+		arrows[0].setVisibility(View.VISIBLE);
 	}
 
 	// Method for setting up the buttons
@@ -156,67 +159,44 @@ public class Level1Activity extends Activity {
 		buttonA.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if (sequence[current] == T_BASE) {
-					genes_ans[current].setVisibility(View.VISIBLE);
-					current++;
-					score++;
-					showScore();
-				} else {
-					timeleft -= PENALTY;
-				}
-				if (current >= ELEM_NUM) {
-					generateSequence();
-				}
+				clickButton(T_BASE);
 			}
 		});
 		buttonT.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if (sequence[current] == A_BASE) {
-					genes_ans[current].setVisibility(View.VISIBLE);
-					current++;
-					score++;
-					showScore();
-				} else {
-					timeleft -= PENALTY;
-				}
-				if (current >= ELEM_NUM) {
-					generateSequence();
-				}
+				clickButton(A_BASE);
 			}
 		});
 		buttonG.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if (sequence[current] == C_BASE) {
-					genes_ans[current].setVisibility(View.VISIBLE);
-					current++;
-					score++;
-					showScore();
-				} else {
-					timeleft -= PENALTY;
-				}
-				if (current >= ELEM_NUM) {
-					generateSequence();
-				}
+				clickButton(C_BASE);
 			}
 		});
 		buttonC.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if (sequence[current] == G_BASE) {
-					genes_ans[current].setVisibility(View.VISIBLE);
-					current++;
-					score++;
-					showScore();
-				} else {
-					timeleft -= PENALTY;
-				}
-				if (current >= ELEM_NUM) {
-					generateSequence();
-				}
+				clickButton(G_BASE);
 			}
 		});
+	}
+	
+	// Button check method
+	public void clickButton(int base) {
+		if (sequence[current] == base) {
+			genes_ans[current].setVisibility(View.VISIBLE);
+			arrows[current++].setVisibility(View.INVISIBLE);
+			score++;
+			showScore();
+		} else {
+			timeleft -= PENALTY;
+		}
+		if (current >= ELEM_NUM) {
+			generateSequence();
+		} else {
+			arrows[current].setVisibility(View.VISIBLE);
+		}
 	}
 
 	// Method for time update
@@ -234,15 +214,42 @@ public class Level1Activity extends Activity {
 	// Finish game
 	public void endLevel() {
 		showTime(0);
+		mp.stop();
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage("The level has finished, your score is " + score)
+		builder.setMessage("CONGRATULATIONS, 1ST LEVEL FINISHED!\n           YOUR SCORE IS " + score)
 		       .setCancelable(false)
 		       .setNeutralButton("Proceed", new DialogInterface.OnClickListener() {
 		           public void onClick(DialogInterface dialog, int id) {
-		                Level1Activity.this.finish();
+		               Level1Activity.this.finish();
 		           }
 		       });
 		AlertDialog alert = builder.create();
 		alert.show();
+	}
+	
+	// Array initialisation method for the layout
+	public void initArrays() {
+		sequence = new int[ELEM_NUM];
+		genes = new ImageView[ELEM_NUM];
+		genes_ans = new ImageView[ELEM_NUM];
+		arrows = new ImageView[ELEM_NUM];
+		genes[0] = (ImageView) findViewById(R.id.gene1);
+		genes[1] = (ImageView) findViewById(R.id.gene2);
+		genes[2] = (ImageView) findViewById(R.id.gene3);
+		genes[3] = (ImageView) findViewById(R.id.gene4);
+		genes[4] = (ImageView) findViewById(R.id.gene5);
+		genes[5] = (ImageView) findViewById(R.id.gene6);
+		genes_ans[0] = (ImageView) findViewById(R.id.gene1_ans);
+		genes_ans[1] = (ImageView) findViewById(R.id.gene2_ans);
+		genes_ans[2] = (ImageView) findViewById(R.id.gene3_ans);
+		genes_ans[3] = (ImageView) findViewById(R.id.gene4_ans);
+		genes_ans[4] = (ImageView) findViewById(R.id.gene5_ans);
+		genes_ans[5] = (ImageView) findViewById(R.id.gene6_ans);
+		arrows[0] = (ImageView) findViewById(R.id.arrow1);
+		arrows[1] = (ImageView) findViewById(R.id.arrow2);
+		arrows[2] = (ImageView) findViewById(R.id.arrow3);
+		arrows[3] = (ImageView) findViewById(R.id.arrow4);
+		arrows[4] = (ImageView) findViewById(R.id.arrow5);
+		arrows[5] = (ImageView) findViewById(R.id.arrow6);
 	}
 }
